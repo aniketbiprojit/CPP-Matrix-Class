693304ae0c9a587847b4ec7c05212c0b9ec8a48c
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2020-03-16 22:18:50 +0530

    cpp20matrix done

--- include/cpp20matrix.hpp
+++ include/cpp20matrix.hpp
@@ -1,13 +1,14 @@
 #ifndef CPP20MATRIX_H
 #define CPP20MATRIX_H 1
 #include <vector>
+#include <range/v3/all.hpp>
 namespace lib
 {
 template <typename T>
 class cpp20matrix
 {
 public:
-    std::vector<std::vector<T>> vec;
+    std::vector<T> vec;
     cpp20matrix(std::initializer_list<std::initializer_list<T>> l)
     {
         vec.clear();
@@ -15,10 +16,11 @@ public:
         {
             std::vector<T> temp;
             for (auto inner_it = it->begin(); inner_it < it->end(); inner_it++)
-                temp.push_back(*inner_it);
-            vec.push_back(temp);
+                vec.push_back(*inner_it);
         }
     }
+
+    
 };
 }// namespace lib
 #endif // CPP20MATRIX_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 #include <vector>
-#include "etmatrix.hpp"
 #include "cpp20matrix.hpp"
+//#include "etmatrix.hpp"
 // #include <exceptional/ranges>
+
+template <typename T>
+auto operator+(const lib::cpp20matrix<T>& left,const lib::cpp20matrix<T>& right){
+    auto sum = ranges::views::zip_with(std::plus{},left.vec,right.vec);
+    return sum;
+}
 
 int main(int argc, char *argv[])
 {
 	std::cout << "C++ version\nBuilt on Version: 201703\nCurrent Version\t: " << __cplusplus << std::endl;
 
-	lib::etmatrix<float> e_mat({{-1, 2, 3, 7.5}, {3, 4, 12, (float)17 / 3}});
-	lib::etmatrix<float> e_mat_1({{2, 4, 6, -18}, {3.2, (float)4 / 7, 16, 17}});
-	lib::etmatrix<float> e;
-	e = (e_mat + e_mat_1);
-	std::cout << e_mat << "\n"
-			  << e_mat_1 << "\n"
-			  << e;
+//	lib::etmatrix<float> e_mat({{-1, 2, 3, 7.5}, {3, 4, 12, (float)17 / 3}});
+//	lib::etmatrix<float> e_mat_1({{2, 4, 6, -18}, {3.2, (float)4 / 7, 16, 17}});
+//	lib::etmatrix<float> e;
+//	e = (e_mat + e_mat_1);
+//	std::cout << e_mat << "\n"
+//			  << e_mat_1 << "\n"
+//			  << e;
+    
+    lib::cpp20matrix <float> cpp_mat({{-1, 2, 3, 7.5}, {3, 4, 12, (float)17 / 3}});
+    lib::cpp20matrix <float> cpp_mat_1({{2, 4, 6, -18}, {3.2, (float)4 / 7, 16, 17}});
+    auto c = cpp_mat+cpp_mat_1;
+    
+    for(int i=0;i<c.size();i++)
+        std::cout << c[i] << " " ;
 }
